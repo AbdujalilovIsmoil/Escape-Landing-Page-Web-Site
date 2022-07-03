cb1f36667958ca0ac6e59f9d41ff6463fcb84629
= <ismoilbek20050421@gmail.com>
2022-07-03 12:26:16 +0500

    Escape

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,8 @@
 // ESCAPE WEB-SITE TO THE CODES START 
+
+window.addEventListener("load",()=>{
+  window.addEventListener("scroll",()=>{
+    let header = document.querySelector(".header");
+    header.classList.toggle("active",scrollY > 0);
+  });
+});
