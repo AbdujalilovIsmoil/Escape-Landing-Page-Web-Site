9ee3b9d4609adc42648573719371a4816a42b0cc
= <ismoilbek20050421@gmail.com>
2022-07-27 16:03:48 +0500

    Escape

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,36 @@
-// ESCAPE WEB-SITE TO THE CODES START 
+// ESCAPE WEB-SITE TO THE CODES START
 
-window.addEventListener("load",()=>{
-  window.addEventListener("scroll",()=>{
+window.addEventListener("load", () => {
+  window.addEventListener("scroll", () => {
     let header = document.querySelector(".header");
-    header.classList.toggle("active",scrollY > 0);
+    header.classList.toggle("active", scrollY > 0);
   });
+
+  let faBarsButton = document.querySelector(".fa-bars");
+  let timesButton = document.querySelector(".fa-times");
+  let secretNavbar = document.querySelector(".secret");
+  let linkButtons = document.querySelectorAll(".secret__link");
+
+  faBarsButton.addEventListener("click", openButton);
+
+  function openButton() {
+    secretNavbar.classList.add("active");
+  }
+
+  timesButton.addEventListener("click", closeButton);
+
+  function closeButton() {
+    secretNavbar.classList.remove("active");
+  }
+
+  for (let i = 0; i < linkButtons.length; i++) {
+    linkButtons[i].addEventListener("click", linkButtonCloseNavbar);
+  }
+
+  function linkButtonCloseNavbar() {
+    secretNavbar.classList.remove("active");
+  }
+
+  // AOS LIBRARY TO THE CODE
+  AOS.init();
 });
