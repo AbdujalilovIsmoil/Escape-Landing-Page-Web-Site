cc0ad0043b2f6dc81db7570ecd6837c9eee71d83
= <ismoilbek20050421@gmail.com>
2022-12-20 13:11:43 +0500

    Escape Web Site The End

--- JS/index.js
+++ JS/index.js
@@ -2,37 +2,30 @@
 
 window.addEventListener("load", () => {
   window.addEventListener("scroll", () => {
-    let header = document.querySelector(".header");
-    header.classList.toggle("active", scrollY > 0);
+    $(".header").classList.toggle("active", scrollY > 0);
   });
 
-  let faBarsButton = document.querySelector(".fa-bars");
-  let timesButton = document.querySelector(".fa-times");
-  let secretNavbar = document.querySelector(".secret");
-  let linkButtons = document.querySelectorAll(".secret__link");
-  let body = document.querySelector("body");
-
-  faBarsButton.addEventListener("click", openButton);
+  $(".fa-bars").addEventListener("click", openButton);
 
   function openButton() {
-    secretNavbar.classList.add("active");
-    body.style.overflow = "hidden";
+    $(".secret").classList.add("active");
+    $("body").style.overflow = "hidden";
   }
 
-  timesButton.addEventListener("click", closeButton);
+  $(".fa-times").addEventListener("click", closeButton);
 
   function closeButton() {
-    secretNavbar.classList.remove("active");
-    body.style.overflow = "";
+    $(".secret").classList.remove("active");
+    $("body").style.overflow = "";
   }
 
-  for (let i = 0; i < linkButtons.length; i++) {
-    linkButtons[i].addEventListener("click", linkButtonCloseNavbar);
+  for (let i = 0; i < $$(".secret__link").length; i++) {
+    $$(".secret__link")[i].addEventListener("click", linkButtonCloseNavbar);
   }
 
   function linkButtonCloseNavbar() {
-    secretNavbar.classList.remove("active");
-    body.style.overflow = "";
+    $(".secret").classList.remove("active");
+    $("body").style.overflow = "";
   }
 
   // AOS LIBRARY TO THE CODE
